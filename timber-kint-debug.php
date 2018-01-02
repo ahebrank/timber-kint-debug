c4f5b868496f9b9afc69a1ff0cffc2b3a7f10cb9
Andy Hebrank <ahebrank@gmail.com>
2018-01-02 10:21:05 -0500

    add depth param

--- timber-kint-debug.php
+++ timber-kint-debug.php
@@ -9,19 +9,43 @@
  * */
 defined( 'ABSPATH' ) or die( 'Nope, not accessing this' );
 class Timber_Kint_Debug {
+  /**
+   * set max depth for kint traversal
+   *
+   * @var integer
+   */
+  private $maxLevels = 4;
+
   function __construct() {
     add_filter('get_twig', array($this,'add_to_twig'));
   }
+
+  /**
+   * add the function to twig
+   *
+   * @param $twig
+   * @return void
+   */
   function add_to_twig($twig) {
     $twig->addExtension(new Twig_Extension_StringLoader());
     $twig->addFunction(new Twig_SimpleFunction('kint', array($this, 'call_kint'),  array('is_safe' => array('html'), 'needs_context' => true, 'needs_environment' => true) ));
     return $twig;
   }
+
+  /**
+   * run kint
+   *
+   * @param $twig
+   * @param $vars
+   * @param boolean $context
+   * @return void
+   */
   function call_kint($twig, $vars, $context = FALSE) {
     if ($twig->isDebug()) {
       if (!class_exists('Kint')) {
         require_once(__DIR__ . '/vendor/autoload.php');
       }
+      Kint::$maxLevels = $this->maxLevels;
       if ($context) {
         Kint::dump($context);
       } else {
